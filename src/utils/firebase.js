8c0fdcbea8c37e7b66cdf792e86bc4f2b64ec1c1
Atokkacakk <atokzzz04@gmail.com>
2025-01-14 19:45:53 +0800

    first commit

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -1,17 +1,18 @@
 import { initializeApp } from "firebase/app";
 import { getDatabase } from "firebase/database";
+require("dotenv").config();
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDer8Ag5FGBq7zjZ3j-tp7YyaXKKWhueps",
-  authDomain: "database-supplier-hi-technics.firebaseapp.com",
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
   databaseURL:
-    "https://database-supplier-hi-technics-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "database-supplier-hi-technics",
-  storageBucket: "database-supplier-hi-technics.firebasestorage.app",
-  messagingSenderId: "934530752707",
-  appId: "1:934530752707:web:1ef00dd9d3995cabac8276",
-  measurementId: "G-P0C4CNLYW3",
+    process.env.DATABASE_URL,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  appId: process.env.APP_ID,
+  measurementId: process.env.MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
